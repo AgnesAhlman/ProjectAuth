71dff378e873310bb8ba81484bb75efa1c05c29a
Agnes Ahlman <ahlman.agnes@gmail.com>
2023-12-11 20:21:59 +0100

    basic Book setup

---
+++ backend/Controllers/BooksController.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BookApi.Models;
+
+namespace BookApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class BooksController : ControllerBase
+  {
+    private readonly BookContext _context;
+
+    public BooksController(BookContext context)
+    {
+      _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
+    {
+
+      var books = await _context.Books.ToListAsync();
+      return books;
+    }
+
+    // GET: api/TodoItems/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Book>> GetSingleBook(long id)
+    {
+      var book = await _context.Books.FindAsync(id);
+
+      if (book == null)
+      {
+        return NotFound();
+      }
+
+      return book;
+    }
+
+  }
+}
